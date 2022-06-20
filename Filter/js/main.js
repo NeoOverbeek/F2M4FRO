1de31c128d205f4751efc0d47f5161da8a0b9f1e
Neo <32793@ma-web.nl>
2022-06-20 09:38:39 +0200

    alles van fro

---
+++ Filter/js/main.js
@@ -0,0 +1,12 @@
+let allGames = document.getElementsByClassName("game");
+
+for (let i = 0; i < allGames.length; i++) {
+
+}
+
+let filters = document.getElementsByClassName("filter");
+filters[0].onchange = function() {
+    if (filters[0].unchecked) {
+        allGames.
+    }
+}
